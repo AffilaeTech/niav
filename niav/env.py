55e1c771134c0f330ff221b4ea98f9f034331f28
Frédéric Dogimont <frederic@affilae.com>
2017-10-19 18:19:11 +0200

    add some logs

--- niav/env.py
+++ niav/env.py
@@ -80,8 +80,10 @@ class Env(object):
         """
         section, key = mixed_key.split(".")
         if section not in self.config.sections():
+            self.log.info("section '%s' not found" % section)
             return None
         elif key not in self.config.options(section):
+            self.log.info("key '%s' not found in section '%s'" % (key, section))
             return None
         return self.config.get(section, key)
 
@@ -114,7 +116,9 @@ class Env(object):
         """
         section, key = mixed_key.split(".")
         if section not in self.config.sections():
+            self.log.info("section '%s' not found" % section)
             return None
         elif key not in self.config.options(section):
+            self.log.info("key '%s' not found in section '%s'" % (key, section))
             return None
         return self.config.getint(section, key)
